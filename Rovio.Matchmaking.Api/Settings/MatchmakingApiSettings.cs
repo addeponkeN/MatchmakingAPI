c2ffbf77d2ebdcbf7b570496425e73496bb58eeb
addeponken <addeponken@hotmail.com>
2022-11-02 12:22:56 +0100

    added build bats & small settings fix.

--- Rovio.Matchmaking.Api/Settings/MatchmakingApiSettings.cs
+++ Rovio.Matchmaking.Api/Settings/MatchmakingApiSettings.cs
@@ -9,6 +9,6 @@ public class MatchmakingApiSettings
         ListenUrls = new() {"https://*:5000", "http://*:5001"}
     };
 
-    [JsonPropertyName("listenurls")]
-    public List<string> ListenUrls { get; init; }
+    [JsonPropertyName("listenurls")] 
+    public List<string> ListenUrls { get; init; } = new();
 }
